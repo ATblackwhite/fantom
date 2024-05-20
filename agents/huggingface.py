b1df61a45323e1c85551e1b503d4404850fc2af0
skywalker023 <hyunwookim.ai@gmail.com>
2024-05-19 21:57:42 -0700

    Update new agents

--- agents/huggingface.py
+++ agents/huggingface.py
@@ -1,79 +1,172 @@
 import torch
-from transformers import AutoTokenizer, AutoModelForCausalLM
-from transformers import T5Tokenizer, T5ForConditionalGeneration
+from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
+from .base import BaseAgent
 
-class HuggingFaceAgent():
-    def __init__(self, args):
-        self.args = args
+class HuggingFaceAgent(BaseAgent):
+    def __init__(self, **kwargs):
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.batch_size = 8 #kwargs['batch_size']
 
-    def preprocess_text(self, text):
+    def init_pipeline(self):
+        self.pipe = pipeline(
+            "text-generation",
+            model=self.model,
+            tokenizer=self.tokenizer,
+            batch_size=self.batch_size,
+        )
+        self.pipe.tokenizer.padding_side = "left"
+
+    def preprocess_input(self, text):
         return text
 
     def postprocess_output(self, response):
         return response
 
-    def interact(self, text):
-        prompt = self.preprocess_text(text)
-        encoded_texts = self.tokenizer(prompt, truncation=True, return_tensors="pt", max_length=512)
-        input_ids = encoded_texts['input_ids'].to(self.device)
-        attention_mask = encoded_texts['attention_mask'].to(self.device)
-        with torch.no_grad():
-            output = self.model.generate(input_ids, attention_mask=attention_mask, max_new_tokens=256)
-        decoded_output = self.tokenizer.decode(output[0], skip_special_tokens=True)
-        response = self.postprocess_output(decoded_output)
+    def postprocess_pipeline_output(self, output):
+        return output[0]['generated_text'].strip()
 
-        return response
+    def encode(self, texts):
+        prompts = [self.preprocess_input(text) for text in texts]
+        encoded_texts = self.tokenizer(prompts, add_special_tokens=False, max_length=512, padding='max_length', truncation=True, return_tensors="pt").to(self.model.device)
+        return encoded_texts
 
-    def batch_interact(self, batch_texts):
-        batch_prompts = [self.preprocess_text(text) for text in batch_texts]
-        encoded_texts = self.tokenizer(batch_prompts, padding=True, truncation=True, return_tensors="pt", max_length=512)
-        input_ids = encoded_texts['input_ids'].to(self.device)
-        attention_mask = encoded_texts['attention_mask'].to(self.device)
-        with torch.no_grad():
-            outputs = self.model.generate(input_ids, attention_mask=attention_mask, max_new_tokens=256)
+    def decode(self, outputs):
         decoded_outputs = self.tokenizer.batch_decode(outputs, skip_special_tokens=True)
         responses = [self.postprocess_output(decoded_output) for decoded_output in decoded_outputs]
+        return responses
+
+    def raw_batch_interact(self, texts, do_sample=True):
+        encoded_texts = self.encode(texts)
+        with torch.no_grad():
+            outputs = self.model.generate(**encoded_texts, max_new_tokens=365, do_sample=do_sample)
+        responses = self.decode(outputs)
+        return responses
+
+    def batch_interact(self, texts, do_sample=True):
+        prompts = [self.preprocess_input(text) for text in texts] # XXX: apply those chat-specific templates beforehand and make them into pipeline batch and directly feed the pipeline
+        outputs = self.pipe(prompts, return_full_text=False, max_new_tokens=365, do_sample=True)
+        responses = [self.postprocess_pipeline_output(output) for output in outputs]
 
         return responses
 
-class FlanT5Agent(HuggingFaceAgent):
-    def __init__(self, args):
-        super().__init__(args)
-        self.tokenizer = T5Tokenizer.from_pretrained("google/" + args.model)
-        self.model = T5ForConditionalGeneration.from_pretrained("google/" + args.model, device_map="auto")
+    def interact(self, text, do_sample=True):
+        return self.batch_interact([text], do_sample)[0]
 
-class FlanUL2Agent(HuggingFaceAgent):
-    def __init__(self, args):
-        super().__init__(args)
-        self.tokenizer = AutoTokenizer.from_pretrained("google/flan-ul2")
-        self.model = T5ForConditionalGeneration.from_pretrained("google/flan-ul2", device_map="auto", load_in_8bit=True)
+    def batch_compute_likelihood(self, input_texts, target_data):
+        """ Compute the log-likelihood of the target data given the input text. """
+        # We should pad after concatenating with target_outputs
+        prompts = [self.preprocess_input(text) for text in input_texts] # apply those chat-specific templates
+        data_appended_prompt = [p + d for p, d in zip(prompts, target_data)] # append the target responses to the prompts
+        encoded_texts = self.tokenizer(data_appended_prompt, add_special_tokens=False, max_length=512, padding='max_length', truncation=True, return_tensors="pt").to(self.model.device)
+        encoded_data = self.tokenizer(target_data, add_special_tokens=False, max_length=512, padding='max_length', truncation=True, return_tensors="pt").to(self.model.device) # this is actually for getting the attention mask to know which part of the input is the response
 
-class MistralAIAgent(HuggingFaceAgent):
-    def __init__(self, args):
-        super().__init__(args)
-        if 'instruct' in self.args.model.lower():
-            model_name = "Mistral-7B-Instruct-v0.1"
-        else:
-            model_name = "Mistral-7B-v0.1"
-        self.tokenizer = AutoTokenizer.from_pretrained("mistralai/" + model_name)
-        self.model = AutoModelForCausalLM.from_pretrained("mistralai/" + model_name, device_map="auto")
-        self.tokenizer.pad_token = self.tokenizer.eos_token 
+        with torch.no_grad():
+            outputs = self.model(**encoded_texts, return_dict=True)
 
-    def preprocess_text(self, text):
-        return self.tokenizer.apply_chat_template([{"role": "user", "content": text}], tokenize=False, add_generation_prompt=True)
+        vocab_distribution = torch.log_softmax(outputs.logits, dim=-1)
+        data_token_logprobs = torch.gather(vocab_distribution[:,:-1,:], 2, encoded_data.input_ids.unsqueeze(-1)[:,1:,:])
+        true_data_token_logprobs = (data_token_logprobs * encoded_data.attention_mask.unsqueeze(-1)[:, 1:, :]).squeeze(-1) # get only the logprobs of the response tokens
+        data_log_likelihood = true_data_token_logprobs.sum(dim=1) / encoded_data.attention_mask.sum(dim=1)
+
+        return data_log_likelihood
+    
+    def compute_data_likelihood(self, input_text, target_datum):
+        return self.batch_compute_likelihood([input_text], [target_datum])[0]
+
+
+class HuggingFaceChatAgent(HuggingFaceAgent):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.model_output_token = "[/INST]"
+
+    def preprocess_input(self, text):
+        messages = [
+        # {"role": "system", "content": "You are a helpful assistant."},
+          {"role": "user", "content": text},
+        ]
+        chat_prompt = self.tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
+        return chat_prompt
 
     def postprocess_output(self, response):
-        return response.split("[/INST]")[-1].strip()
+        return response.split(self.model_output_token)[-1].strip()
 
-class ZephyrAgent(HuggingFaceAgent):
-    def __init__(self, args):
-        super().__init__(args)
-        self.tokenizer = AutoTokenizer.from_pretrained("HuggingFaceH4/" + self.args.model)
-        self.model = AutoModelForCausalLM.from_pretrained("HuggingFaceH4/" + self.args.model, device_map="auto")
+class Llama2Agent(HuggingFaceAgent):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        assert kwargs['model_size'].lower() in ['7b', '13b', '70b']
+        self.tokenizer = AutoTokenizer.from_pretrained(f"meta-llama/Llama-2-{kwargs['model_size'].lower()}-hf", padding_side='left')
+        self.model = AutoModelForCausalLM.from_pretrained(f"meta-llama/Llama-2-{kwargs['model_size'].lower()}-hf", device_map="auto",
+                                                          torch_dtype=torch.float16, attn_implementation="flash_attention_2")
+        self.tokenizer.pad_token = self.tokenizer.eos_token # LLaMa tokenizer has no pad token
+        self.init_pipeline()
+        self.pipe.tokenizer.pad_token_id = self.model.config.eos_token_id
 
-    def preprocess_text(self, text):
-        return self.tokenizer.apply_chat_template([{"role": "user", "content": text}], tokenize=False, add_generation_prompt=True)
+class Llama2ChatAgent(HuggingFaceChatAgent):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        assert kwargs['model_size'].lower() in ['7b', '13b', '70b']
+        self.tokenizer = AutoTokenizer.from_pretrained(f"meta-llama/Llama-2-{kwargs['model_size'].lower()}-chat-hf", padding_side='left')
+        self.model = AutoModelForCausalLM.from_pretrained(f"meta-llama/Llama-2-{kwargs['model_size'].lower()}-chat-hf", device_map="auto",
+                                                          torch_dtype=torch.float16, attn_implementation="flash_attention_2")
+        self.tokenizer.pad_token = self.tokenizer.eos_token # LLaMa tokenizer has no pad token
+        self.model_output_token = "[/INST]"
+        self.init_pipeline()
+        self.pipe.tokenizer.pad_token_id = self.model.config.eos_token_id
 
-    def postprocess_output(self, response):
-        return response.split("\n<|assistant|>\n")[-1].strip()
+class MistralAgent(HuggingFaceAgent):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-v0.1", padding_side='left')
+        self.model = AutoModelForCausalLM.from_pretrained("mistralai/Mistral-7B-v0.1", device_map="auto",
+                                                          torch_dtype=torch.float16, attn_implementation="flash_attention_2")
+        self.tokenizer.pad_token = self.tokenizer.eos_token # LLaMa tokenizer has no pad token
+        # self.init_pipeline()
+        # self.pipe.tokenizer.pad_token_id = self.model.config.eos_token_id
+
+class MistralInstructAgent(HuggingFaceChatAgent):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-Instruct-v0.2", padding_side='left')
+        self.model = AutoModelForCausalLM.from_pretrained("mistralai/Mistral-7B-Instruct-v0.2", device_map="auto",
+                                                          torch_dtype=torch.float16, attn_implementation="flash_attention_2")
+        self.tokenizer.pad_token = self.tokenizer.eos_token # LLaMa tokenizer has no pad token
+        self.model_output_token = "[/INST]"
+        self.init_pipeline()
+        self.pipe.tokenizer.pad_token_id = self.model.config.eos_token_id
+
+class MixtralInstructAgent(HuggingFaceChatAgent):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.tokenizer = AutoTokenizer.from_pretrained("mistralai/Mixtral-8x7B-Instruct-v0.1", padding_size='left')
+        self.model = AutoModelForCausalLM.from_pretrained("mistralai/Mixtral-8x7B-Instruct-v0.1", device_map="auto",
+                                                          torch_dtype=torch.float16, attn_implementation="flash_attention_2")
+        self.tokenizer.pad_token = self.tokenizer.eos_token # LLaMa tokenizer has no pad token
+        self.model_output_token = "[/INST]"
+
+class ZephyrAgent(HuggingFaceChatAgent):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.tokenizer = AutoTokenizer.from_pretrained("HuggingFaceH4/zephyr-7b-beta", padding_side='left')
+        self.model = AutoModelForCausalLM.from_pretrained("HuggingFaceH4/zephyr-7b-beta", device_map="auto",
+                                                          torch_dtype=torch.float16, attn_implementation="flash_attention_2")
+        self.model_output_token = "<|assistant|>"
+        self.init_pipeline()
+
+class GemmaAgent(HuggingFaceAgent):
+    def __init__(self, **kwargs):
+        assert kwargs['model_size'].lower() in ['2b', '7b']
+        super().__init__(**kwargs)
+        self.tokenizer = AutoTokenizer.from_pretrained(f"google/gemma-{kwargs['model_size'].lower()}", padding_side='left')
+        self.model = AutoModelForCausalLM.from_pretrained(f"google/gemma-{kwargs['model_size'].lower()}", device_map="auto",
+                                                          torch_dtype=torch.float16, attn_implementation="flash_attention_2")
+        self.init_pipeline()
+
+class GemmaInstructAgent(HuggingFaceChatAgent):
+    def __init__(self, **kwargs):
+        assert kwargs['model_size'].lower() in ['2b', '7b']
+        super().__init__(**kwargs)
+        self.tokenizer = AutoTokenizer.from_pretrained(f"google/gemma-{kwargs['model_size'].lower()}-it", padding_side='left')
+        self.model = AutoModelForCausalLM.from_pretrained(f"google/gemma-{kwargs['model_size'].lower()}-it", device_map="auto",
+                                                          torch_dtype=torch.float16, attn_implementation="flash_attention_2")
+        self.model_output_token = "\nmodel\n"
+        self.init_pipeline()
--- agents/together_ai.py
+++ agents/together_ai.py
@@ -1,61 +1,71 @@
 import os
-import time
-import requests
-from types import SimpleNamespace
 import together
+from types import SimpleNamespace
+from .base import BaseAgent, AsyncBaseAgent
 
-class TogetherAIAgent():
+class TogetherAIAgent(BaseAgent):
     def __init__(self, kwargs: dict):
         self.api_key = together.api_key = os.getenv('TOGETHERAI_API_KEY')
         self.args = SimpleNamespace(**kwargs)
         self._set_default_args()
-        self.args.model = "togethercomputer/" + self.args.model.removesuffix("-tg")
+        self.args.model = self.args.model.removesuffix("-tg")
 
     def _set_default_args(self):
-        if not hasattr(self.args, 'model'):
-            self.args.model = "togethercomputer/llama-2-70b-chat"
         if not hasattr(self.args, 'temperature'):
-            self.args.temperature = 0.0
+            self.args.temperature = 1.0
         if not hasattr(self.args, 'max_tokens'):
             self.args.max_tokens = 256
         if not hasattr(self.args, 'top_p'):
-            self.args.top_p = 0.95
-        if not hasattr(self.args, 'repetition_penalty'):
-            self.args.repetition_penalty = 1.0
-
-    def generate(self, prompt):
-        while True:
-            try:
-                output = together.Complete.create(
-                    prompt = prompt, 
-                    model = self.args.model, 
-                    max_tokens = self.args.max_tokens,
-                    temperature = self.args.temperature,
-                    top_k = 1,
-                    top_p = self.args.top_p,
-                    repetition_penalty = 1.0,
-                    stop = ['</s>']
-                )
-                break
-            except (requests.exceptions.ConnectionError, requests.exceptions.ReadTimeout, requests.exceptions.JSONDecodeError) as e:
-                print("Error: {}\nRetrying...".format(e))
-                time.sleep(2)
-                continue
+            self.args.top_p = 1.0
+        if not hasattr(self.args, 'frequency_penalty'):
+            self.args.frequency_penalty = 0
+        if not hasattr(self.args, 'presence_penalty'):
+            self.args.presence_penalty = 0
+    
+    def generate(self, prompt, temperature=None, max_tokens=None):
+        output = together.Complete.create(
+            prompt=prompt,
+            model=self.args.model,
+            max_tokens = self.args.max_tokens if max_tokens is None else max_tokens,
+            temperature = self.args.temperature if temperature is None else temperature,
+        )
 
         return output
 
-    def parse_basic_text(self, response):
-        return response['output']['choices'][0]['text'].strip()
+    def postprocess_output(self, output):
+        responses = [c['text'].strip() for c in output['output']['choices']]
+        return responses[0]
 
-    def interact(self, prompt):
-        while True:
-            try:
-                response = self.generate(prompt)
-                output = self.parse_basic_text(response)
-                break
-            except:
-                print("Error: Retrying...")
-                time.sleep(2)
-                continue
+    def preprocess_input(self, text):
+        return text
+
+    def interact(self, prompt, temperature=None, max_tokens=None):
+        output = self.generate(prompt, temperature=temperature, max_tokens=max_tokens)
+        response = self.postprocess_output(output)
+
+        return response
+
+class AsyncTogetherAIAgent(AsyncBaseAgent):
+    def __init__(self, kwargs: dict):
+        super().__init__()
+        self.api_key = together.api_key = os.getenv('TOGETHERAI_API_KEY')
+        self.args = SimpleNamespace(**kwargs)
+        self._set_default_args()
+        self.args.model = self.args.model.removesuffix("-tg")
+
+    def generate(self, prompt, temperature=None, max_tokens=None):
+        output = together.Complete.create(
+            prompt=prompt,
+            model=self.args.model,
+            max_tokens = self.args.max_tokens if max_tokens is None else max_tokens,
+            temperature = self.args.temperature if temperature is None else temperature,
+        )
 
         return output
+
+    def postprocess_output(self, output):
+        responses = [c['text'].strip() for c in output['output']['choices']]
+        return responses[0]
+
+    def preprocess_input(self, text):
+        return text
